9491a20d55f8db29fc2af016c45362ea168f80df
Rayssa Cadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-01-24 12:56:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
 server.use(router)
+server.use(middlewares)
 
 server.listen(3000, () => { console.log('JSON Server está rodando') })
