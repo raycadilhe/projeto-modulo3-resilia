ada0c9b012cdb094aa9a5fa24367990ee17d811d
Rayssa <raycadilhe@hotmail.com>
2023-01-23 21:48:35 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+server.use(router)
+
+server.listen(3000, () => { console.log('JSON Server está rodando') })
